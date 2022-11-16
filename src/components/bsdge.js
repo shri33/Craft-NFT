44fffd83556571e795ae4200dc68d50e54a3f457
Shri Srivastava <76124911+shri33@users.noreply.github.com>
2022-11-16 23:57:09 +0530

    Add files via upload

---
+++ src/components/bsdge.js
@@ -0,0 +1,17 @@
+// @src/theme/components/badge.js
+
+export const badgeStyles = {
+    components: {
+        Badge: {
+            sizes: {
+                md: {
+                    width: "65px",
+                    height: "25px"
+                }
+            },
+            baseStyle: {
+                textTransform: "capitalize"
+            }
+        }
+    }
+}
